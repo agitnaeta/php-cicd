1656696fb06d7360c14fb71df43fa798bc0287db
agitnaeta <agitnaeta@gmail.com>
2024-11-15 00:44:23 +0700

    Update Text

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ use Carbon\Carbon;
 
 $time = Carbon::now();
 echo "Hallo Jhon<br>";
-echo "Testing update<br>";
+echo "Code update<br>";
 echo "Waktu Sekarang: ".$time->format("Y-m-d/m/y H:i")."<br>";
