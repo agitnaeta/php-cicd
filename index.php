b3143fe2cd4932c92f1a2f89970ab767a00c140b
agitnaeta <agitnaeta@gmail.com>
2024-11-15 00:17:02 +0700

    Update Text Per Line

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php 
 
-    include 'vendor/autoload.php';
-    use Carbon\Carbon;
+include 'vendor/autoload.php';
+use Carbon\Carbon;
 
-    $time = Carbon::now();
-    echo "Hallo AGIT\r\n";
-    echo "Testing update\r\n";
-    echo "Waktu Sekarang: ".$time->format("Y-m-d/m/y H:i");
+$time = Carbon::now();
+echo "Hallo AGIT<br>";
+echo "Testing update<br>";
+echo "Waktu Sekarang: ".$time->format("Y-m-d/m/y H:i")."<br>";
