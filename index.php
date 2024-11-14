818620d3d124f7a81df461bd0f09994ad2eced90
agitnaeta <agitnaeta@gmail.com>
2024-11-15 00:26:42 +0700

    Update Name

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ include 'vendor/autoload.php';
 use Carbon\Carbon;
 
 $time = Carbon::now();
-echo "Hallo AGIT<br>";
+echo "Hallo Jhon<br>";
 echo "Testing update<br>";
 echo "Waktu Sekarang: ".$time->format("Y-m-d/m/y H:i")."<br>";
